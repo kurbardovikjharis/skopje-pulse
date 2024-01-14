b0567598a3e31e3bfac165a0af2ffde3ade4e4b7
kurbardovikjharis <kurbardovikjharis@gmail.com>
2024-01-14 01:18:38 +0100

    refactor

--- app/src/main/java/com/haris/skopjepulse/ui/theme/Color.kt
+++ app/src/main/java/com/haris/skopjepulse/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.haris.skopjepulse.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 val md_theme_light_primary = Color(0xFFBD0F1E)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_primaryContainer = Color(0xFFFFDAD6)
